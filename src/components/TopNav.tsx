bd81025327b52b4fe061f711835489caf160af3c
Brent152 <60564806+Brent152@users.noreply.github.com>
2024-04-15 15:15:32 -0700

    redid entire project

---
+++ src/components/TopNav.tsx
@@ -0,0 +1,8 @@
+export function TopNav() {
+    return (
+        <nav className="flex w-full items-center justify-between p-4 text-xl font-semibold border-b">
+            <a href="/">Home</a>
+            <a href="/">{}</a>
+        </nav>
+    );
+}
