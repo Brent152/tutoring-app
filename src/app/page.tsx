10776ca0cab030e1fd8b1369b94f77b150ceaf78
Brent152 <60564806+Brent152@users.noreply.github.com>
2024-04-15 09:01:07 -0700

    added db setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,14 @@
-import Link from "next/link";
+import { db } from "~/server/db";
 
-export default function HomePage() {
+export default async function HomePage() {
+
+  const posts = await db.query.posts.findMany();
+
   return (
     <main className="">
+
+    {posts.map((post) => (<div key={post.id}>{post.name}</div>))}
+
       <div className="bg-neutral-800">
         Hello page!
       </div>
