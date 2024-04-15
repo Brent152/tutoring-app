d4b88822e9e28b258d3752cf1702c0b6d62b3213
Brent152 <60564806+Brent152@users.noreply.github.com>
2024-04-15 09:06:38 -0700

    changed ts version

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,15 @@
 import { db } from "~/server/db";
 
+export const dynamic = "force-dynamic"
+
 export default async function HomePage() {
 
   const posts = await db.query.posts.findMany();
 
   return (
     <main className="">
-
-    {posts.map((post) => (<div key={post.id}>{post.name}</div>))}
-
       <div className="bg-neutral-800">
+        {posts.map((post) => (<div key={post.id}>{post.name}</div>))}
         Hello page!
       </div>
     </main>
